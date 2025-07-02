f6b428b9e65370111692f19c1bc66649d59784bd
saidavali123 <saidavali104@gmail.com>
2025-07-02 13:18:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -515,5 +515,6 @@ def cancel_booking():
     return redirect(url_for('dashboard'))
 
 
-if __name__ == '__main__':
-    pass
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
